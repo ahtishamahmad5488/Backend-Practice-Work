4cb2322277562bafca2a1e379a2e0f71599354c3
Ahtisham Ahmad <ahtisham548849@gmail.com>
2025-07-15 13:09:20 +0500

    Working with External Package

---
+++ colors.js
@@ -0,0 +1,8 @@
+const colors = require("colors");
+
+//--- External Package for Colors
+console.log("This is Ahtisham Ahmad");
+console.log(colors.red("This is Ahtisham Ahmad"));
+console.log(colors.green("This is Ahtisham Ahmad"));
+console.log(colors.blue("This is Ahtisham Ahmad"));
+console.log(colors.magenta("This is Ahtisham Ahmad"));
