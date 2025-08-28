f83168dd700137972760d9d72faf4ac48db5a2d8
Ahtisham Ahmad <ahtisham548849@gmail.com>
2025-08-28 12:22:44 +0500

    Modules & Routes

---
+++ NodeWork.js
@@ -0,0 +1,16 @@
+console.log("Hello, World!");
+console.log("Second Execution, Musaab");
+
+setTimeout(() => {
+  console.log("4th Execution, Arslan");
+}, 5000);
+
+setTimeout(() => {
+  console.log("5th Execution, Ahtisham");
+}, 3000);
+
+setTimeout(() => {
+  console.log("6th Execution, Ali");
+}, 2000);
+
+console.log("3rd Execution, Ahmad");
---
+++ userDataSubmit.js
@@ -0,0 +1,18 @@
+const queryString = require("querystring");
+
+function userDataSubmit(req, resp) {
+  let dataBody = [];
+  req.on("data", (chunk) => {
+    dataBody, push(chunk);
+  });
+  req.on("end", () => {
+    let rawData = Buffer.concat(dataBody).toString();
+    let readableData = queryString.parse(rawData);
+    let dataString =
+      "My Name Is " + readableData.name + ". My Email Is " + readableData.email;
+    console.log(dataString);
+  });
+  resp.write(`<h1>Form submitted successfully!</h1>`);
+}
+
+module.exports = userDataSubmit;
